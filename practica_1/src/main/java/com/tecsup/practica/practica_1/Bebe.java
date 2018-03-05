f83eb13852be0041f3b296b1b33dac8c0fa9f934
Alumno <Alumno@LAB701-19>
2018-03-05 11:10:51 -0500

    Commit_2

---
+++ practica_1/src/main/java/com/tecsup/practica/practica_1/Bebe.java
@@ -0,0 +1,25 @@
+package com.tecsup.practica.practica_1;
+
+public class Bebe implements One {
+
+	public void descripcion() {
+		
+	}
+
+	public void nom_prenda() {
+		
+	}
+
+	public void talla() {
+		
+	}
+	
+	public void precio() {
+		
+	}
+	
+	public void foto() {
+		
+	}
+
+}
--- practica_1/src/main/java/com/tecsup/practica/practica_1/One.java
+++ practica_1/src/main/java/com/tecsup/practica/practica_1/One.java
@@ -2,4 +2,10 @@ package com.tecsup.practica.practica_1;
 
 public interface One {
 
+	public void descripcion();
+	public void nom_prenda();
+	public void talla();
+	public void precio();
+	public void foto();
+	
 }
